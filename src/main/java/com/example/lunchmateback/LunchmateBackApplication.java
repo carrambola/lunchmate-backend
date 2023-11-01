95eb6c69bd82a438400a62aa429954cc2d367786
carrambola <grzesiu.fiedoruk@gmail.com>
2023-11-01 13:17:05 +0100

    Initial commit

---
+++ src/main/java/com/example/lunchmateback/LunchmateBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.lunchmateback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LunchmateBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LunchmateBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lunchmateback/LunchmateBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lunchmateback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LunchmateBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
